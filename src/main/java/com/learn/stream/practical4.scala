8317f2648581eb5a58ac3e226eed0b8538489a28
vijay <vijay2340025@gmail.com>
2023-04-08 14:49:20 +0530

    wordcount structured streaming

---
+++ src/main/java/com/learn/stream/practical4.scala
@@ -0,0 +1,46 @@
+package com.learn.stream
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions.{col, explode, split}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object practical4 {
+    def main(args: Array[String]): Unit = {
+
+        Logger.getLogger("org").setLevel(Level.ERROR)
+        val sparkConf: SparkConf = new SparkConf()
+          .setMaster("local[*]")
+          .setAppName("Structured Streaming")
+
+        val spark: SparkSession = SparkSession.builder()
+          .config(sparkConf)
+          .getOrCreate()
+
+
+        // 1. read stream
+        val df = spark
+          .readStream
+          .format("socket")
+          .option("host", "localhost")
+          .option("port", "3131")
+          .load()
+
+        // 2. processing
+        df.printSchema()
+        val resultDF: DataFrame = df.select(
+            explode(split(col("value"), " "))
+              .as("result")
+        ).groupBy("result").count()
+
+        // 3. write to sink
+        val dfQuery = resultDF
+          .writeStream
+          .format("console")
+          .outputMode("complete")
+          .start()
+
+        dfQuery.awaitTermination()
+        spark.close()
+    }
+}
