c41efc8f712ac3895a25f8f0ba6c99deeb4a197b
vijay <vijay2340025@gmail.com>
2023-04-15 16:45:39 +0530

    Update output mode in spark stream

---
+++ src/main/java/com/learn/stream/practical5.scala
@@ -0,0 +1,43 @@
+package com.learn.stream
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.functions.{col, explode, split}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object practical5 {
+    def main(args: Array[String]): Unit = {
+        Logger.getLogger("org").setLevel(Level.ERROR)
+
+        val spark: SparkSession = SparkSession.builder()
+          .master("local[*]")
+          .appName("Structured Streaming\"")
+          .config("spark.sql.shuffle.partitions", 3)
+          .getOrCreate()
+
+
+        // 1. read stream
+        val df = spark
+          .readStream
+          .format("socket")
+          .option("host", "localhost")
+          .option("port", "3131")
+          .load()
+
+        // 2. processing
+        df.printSchema()
+        val resultDF: DataFrame = df.select(
+            explode(split(col("value"), " "))
+              .as("result")
+        ).groupBy("result").count()
+
+        // 3. write to sink
+        val dfQuery = resultDF
+          .writeStream
+          .format("console")
+          .outputMode("update")
+          .start()
+
+        dfQuery.awaitTermination()
+        spark.close()
+    }
+}
